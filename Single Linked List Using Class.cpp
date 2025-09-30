379a8c4fd7224a4eda507d820aa4ed72270031eb
Siddhesh Gajare <ssgajare12345@gmail.com>
2025-09-30 20:01:53 +0530

    Single Linked List Using Class.cpp

---
+++ Single Linked List Using Class.cpp
@@ -0,0 +1,27 @@
+// Exp - 17
+// Siddhesh
+// 25070123505 (A3)
+
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int val;
+    Node*next;
+    
+    Node(int data){
+        val=data;
+        next=NULL;
+    }
+};
+
+int main() {
+    Node*n=new Node(20);
+    cout<<n->val<<" "<<n->next<<endl;
+
+    return 0;
+}
+
+Output :-
+20 0
